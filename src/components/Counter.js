b40e166ade397fcea1e7410bff74160bd7ec1eb5
SrinivasaKamathR <kamathsri1997@gmail.com>
2023-01-23 13:30:25 +0530

    providing the redux store to react components

---
+++ src/components/Counter.js
@@ -0,0 +1,39 @@
+import { useSelector, useDispatch } from "react-redux";
+import classes from "./Counter.module.css";
+
+const Counter = () => {
+  const dispatch = useDispatch();
+  const counter = useSelector((state) => state.counter);
+
+  const incrementHandler = () => {
+    dispatch({ type: "increment" });
+  };
+  const deccrementHandler = () => {
+    dispatch({ type: "decrement" });
+  };
+
+  const incrementHandler2 = () => {
+    dispatch({ type: "incrementBy2" });
+  };
+
+  const decrementHandler2 = () => {
+    dispatch({ type: "decrementBy2" });
+  };
+  const toggleCounterHandler = () => {};
+
+  return (
+    <main className={classes.counter}>
+      <h1>Redux Counter</h1>
+      <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={deccrementHandler}>Decrement</button>
+        <button onClick={incrementHandler2}>Increment by 2</button>
+        <button onClick={decrementHandler2}>Decrement by 2</button>
+      </div>
+      <button onClick={toggleCounterHandler}>Toggle Counter</button>
+    </main>
+  );
+};
+
+export default Counter;
